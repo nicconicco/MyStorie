d2bd9a03b0de1ccabc8c8f185c1a8ed0b42eccfc
nicconicco <nicolaugalves@gmail.com>
2018-08-31 11:11:39 -0300

    [ Usando roboeletrics para mostrar unitTest ]

--- app/src/main/java/mvvm/feature/login/vm/LoginViewModel.java
+++ app/src/main/java/mvvm/feature/login/vm/LoginViewModel.java
@@ -1,21 +1,17 @@
 package mvvm.feature.login.vm;
 
 import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 
-import com.cgalves.mystorie.MyStorieApplication;
 import com.cgalves.mystorie.common.abstractcalls.LoginAbstractCall;
 import com.cgalves.mystorie.common.factory.APIAbstractFactory;
 import com.cgalves.mystorie.common.model.Error;
 import com.cgalves.mystorie.common.model.User;
 import com.cgalves.mystorie.common.model.UserResponse;
 
-import org.androidannotations.annotations.App;
-import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
---
+++ app/src/test/java/com/mystorie/cgalves/mystorie/LoginViewModelTest.java
@@ -0,0 +1,49 @@
+package com.mystorie.cgalves.mystorie;
+
+import android.arch.lifecycle.ViewModelProviders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import mvvm.feature.login.ui.LoginJaderActivity;
+import mvvm.feature.login.vm.LoginViewModel;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by scopus on 31/08/18.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+public class LoginViewModelTest {
+
+    @Test
+    public void testWorngName() {
+        LoginViewModel vm = getViewModel();
+        vm.login("nicco","senha");
+        assertEquals("nicco", vm.userResponse.getValue().getUser().getName());
+    }
+
+    @Test
+    public void testFakeName() {
+        LoginViewModel vm = getViewModel();
+        vm.login("nicco","senha");
+        assertEquals("Fake Test", vm.userResponse.getValue().getUser().getName());
+    }
+
+    @Test
+    public void testIsAdminFalse() {
+        LoginViewModel vm = getViewModel();
+        vm.login("nicco","senha");
+        assertFalse(vm.userResponse.getValue().getUser().getIsAdmin());
+    }
+
+    private LoginViewModel getViewModel() {
+        LoginJaderActivity activity = Robolectric.setupActivity(LoginJaderActivity.class);
+        return ViewModelProviders.of(activity).get(LoginViewModel.class);
+    }
+}
