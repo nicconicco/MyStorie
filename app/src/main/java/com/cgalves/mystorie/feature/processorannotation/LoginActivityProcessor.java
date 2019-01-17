cfabcdcb81b62cf6ed2530e12b8ebc898b29afc8
nicconicco <nicolaugalves@gmail.com>
2019-01-17 17:19:38 -0200

    [ tirando para ver se builda no teamcity ]

--- app/src/main/java/com/cgalves/mystorie/feature/processorannotation/LoginActivityProcessor.java
+++ app/src/main/java/com/cgalves/mystorie/feature/processorannotation/LoginActivityProcessor.java
@@ -1,34 +1,34 @@
 package com.cgalves.mystorie.feature.processorannotation;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
-import com.cgalves.mystorie.R;
-import com.mindorks.binder.Binding;
-import com.mindorks.lib.annotations.BindView;
-import com.mindorks.lib.annotations.OnClick;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.TextView;
+//
+//import com.cgalves.mystorie.R;
+//import com.mindorks.binder.Binding;
+//import com.mindorks.lib.annotations.BindView;
+//import com.mindorks.lib.annotations.OnClick;
 
 public class LoginActivityProcessor extends AppCompatActivity {
 
-    @BindView(R.id.tv_content)
-    TextView tvContent;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_processor);
-        Binding.bind(this);
-    }
-
-    @OnClick(R.id.bt_1)
-    void bt1Click(View v) {
-        tvContent.setText("Button 1 Clicked");
-    }
-
-    @OnClick(R.id.bt_2)
-    void bt2Click(View v) {
-        tvContent.setText("Button 2 Clicked");
-    }
+//    @BindView(R.id.tv_content)
+//    TextView tvContent;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login_processor);
+//        Binding.bind(this);
+//    }
+//
+//    @OnClick(R.id.bt_1)
+//    void bt1Click(View v) {
+//        tvContent.setText("Button 1 Clicked");
+//    }
+//
+//    @OnClick(R.id.bt_2)
+//    void bt2Click(View v) {
+//        tvContent.setText("Button 2 Clicked");
+//    }
 }
